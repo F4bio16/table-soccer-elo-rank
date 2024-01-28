8af2e0346f83e7642332429efb00c51203680257
Fabio Folchi <folchi@webmonks.it>
2024-01-28 02:04:11 +0100

    create template of player detail

--- api/webclient_route.py
+++ api/webclient_route.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
+from repositories.sqlite_repo import SQLiteRepository
 
-def init(webservice_name):
+def init(webservice_name, repo: SQLiteRepository):
     blueprint = Blueprint('webclient', webservice_name,
         url_prefix="", template_folder='./web-client/')
 
@@ -13,6 +14,17 @@ def init(webservice_name):
     @blueprint.route('/players')
     def player_list():
         return render_template('players.html')
+
+    @blueprint.route('/players/<player_id>')
+    def player_details(player_id):
+        try:
+            player = repo.get_player(player_id, None)
+            if player is None:
+                return 'Not found', 404
+
+            return render_template('player_details.html', player=player)
+        except:
+            return 'Not found', 404
 
     @blueprint.route('/matches')
     def matches_list():
